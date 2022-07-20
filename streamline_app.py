5f6d9763e8e9ad80982cc4eb59036e999cfee07f
enigmantra3 <109636481+enigmantra3@users.noreply.github.com>
2022-07-19 18:18:45 -0700

    added text

--- streamline_app.py
+++ streamline_app.py
@@ -4,6 +4,6 @@ streamlit.title("My Parents' New Healthy Diner")
 
 streamlit.header("Breakfast Menu")
 
-streamlit.header("Eggs and Avocados")
+streamlit.text("Eggs and Avocados")
 
-streamlit.header("Shakshuka")
+streamlit.text("Shakshuka")
