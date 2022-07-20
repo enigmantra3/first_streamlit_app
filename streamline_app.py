97739512697d3019e01644cb8f4fe158d7b1dfa8
enigmantra3 <109636481+enigmantra3@users.noreply.github.com>
2022-07-19 18:19:37 -0700

    course BF menu

--- streamline_app.py
+++ streamline_app.py
@@ -7,3 +7,8 @@ streamlit.header("Breakfast Menu")
 streamlit.text("Eggs and Avocados")
 
 streamlit.text("Shakshuka")
+
+streamlit.header('Breakfast Menu (Course)')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
