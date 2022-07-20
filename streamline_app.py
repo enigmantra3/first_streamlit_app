bfde60627c2fbf6d84cd546d49d4e9ec764c533d
enigmantra3 <109636481+enigmantra3@users.noreply.github.com>
2022-07-19 18:21:05 -0700

    added emojis

--- streamline_app.py
+++ streamline_app.py
@@ -8,7 +8,9 @@ streamlit.text("Eggs and Avocados")
 
 streamlit.text("Shakshuka")
 
+
 streamlit.header('Breakfast Menu (Course)')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
