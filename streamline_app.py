b2507b77732d897c00a8e829fb262201da0741c7
enigmantra3 <109636481+enigmantra3@users.noreply.github.com>
2022-07-21 11:48:33 -0700

    types

--- streamline_app.py
+++ streamline_app.py
@@ -58,9 +58,10 @@ streamlit.dataframe(my_data_row)
 
 fruit_choice2 = streamlit.text_input('Add a fruit?','Kumquat')
 streamlit.write('The user entered ', fruit_choice2)
-my_data_row2 = my_data_row[0].append(fruit_choice2)
+my_data_row2 = my_data_row.append(fruit_choice2)
 
 print(type(my_data_row))
+print(type(my_data_row2))
 streamlit.dataframe(my_data_row2)
 
 
