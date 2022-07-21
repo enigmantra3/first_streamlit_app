53e3041899ecf1595508da621006c66eb978691a
enigmantra3 <109636481+enigmantra3@users.noreply.github.com>
2022-07-21 13:07:02 -0700

    show my_data_rows

--- streamline_app.py
+++ streamline_app.py
@@ -68,3 +68,4 @@ if streamlit.button('Get Fruit Load List'):
     my_data_rows = get_fruit_load_list()
     back_from_function = insert_row_snowflake(fruit_choice2.strip())
     streamlit.text(back_from_function)
+    streamlit.dataframe(my_data_rows)
